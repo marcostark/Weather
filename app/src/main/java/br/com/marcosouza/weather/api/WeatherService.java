88df423a774607b455bee107e4af230fd366b6c8
stark <mrcs.stark@gmail.com>
2019-09-28 12:47:37 -0300

    implement service and client retrofit

--- app/src/main/java/br/com/marcosouza/weather/api/WeatherService.java
+++ app/src/main/java/br/com/marcosouza/weather/api/WeatherService.java
@@ -1,4 +1,16 @@
 package br.com.marcosouza.weather.api;
 
-public class WeatherService {
-}
+import br.com.marcosouza.weather.model.WeatherResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface WeatherService {
+
+    String APPID = "903a7eab47f20e0bd580b60ee7760f14";
+
+        @GET("weather?")
+        Call<WeatherResponse> getCurrentWeatherData(@Query("lat") String lat, @Query("lon") String lon, @Query(APPID) String app_id);
+    }
+
+
--- app/src/main/java/br/com/marcosouza/weather/api/WeatherServiceGenerator.java
+++
@@ -1,4 +0,0 @@
-package br.com.marcosouza.weather.api;
-
-public class WeatherServiceGenerator {
-}
